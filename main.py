2f2a34cb5a61efbe76d4fea97ce6ecf238312393
Fedt2407 <federico.tognetti@gmail.com>
2024-07-12 10:28:02 +0200

    Refactored site structure in one page

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 import numpy as np
 from PIL import Image
 from collections import Counter
 import io
+import os
 
 app = Flask(__name__)
+
+UPLOAD_FOLDER = 'static/uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def upload_file():
@@ -12,45 +16,34 @@ def upload_file():
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    # Checks if the file is uploaded
     if 'file' not in request.files:
         return render_template('upload.html', error='No file part')
 
     file = request.files['file']
 
-    # Checks if the file is empty
     if file.filename == '':
         return render_template('upload.html', error='No selected file')
 
-    # Checks if the file is a valid image file
     if file and allowed_file(file.filename):
-        img_bytes = file.read()
-        img = Image.open(io.BytesIO(img_bytes))
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        file.save(file_path)
         
-        # Converts the image to RGB mode
+        img = Image.open(file_path)
         img_array = np.array(img)
 
-        # Resize the image to 100x100 pixels
-        small_img = img.resize((100, 100))
-        small_img_array = np.array(small_img)
-
-        # Extract the 10 most common colors from the image
-        colors, counts = np.unique(small_img_array.reshape(-1, 3), axis=0, return_counts=True)
-        sorted_indices = np.argsort(-counts)[:10] # Sort in descending order
+        colors, counts = np.unique(img_array.reshape(-1, 3), axis=0, return_counts=True)
+        sorted_indices = np.argsort(-counts)[:10]
         top_colors = colors[sorted_indices]
-        total_pixels = small_img_array.shape[0] * small_img_array.shape[1]
+        total_pixels = img_array.shape[0] * img_array.shape[1]
 
-        # Caculates the percentage of each color
         percentages = [(count / total_pixels) * 100 for count in counts[sorted_indices]]
 
-        # Prepare the data to be displayed
         result_data = [{'color': tuple(color), 'percentage': round(percentage, 2)} for color, percentage in zip(top_colors, percentages)]
 
-        return render_template('result.html', result=result_data)
+        return render_template('upload.html', result=result_data, image_path=file_path)
 
     return render_template('upload.html', error='Invalid file type')
 
-# Checks if the file is a valid image file
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in {'png', 'jpg', 'jpeg', 'gif'}
 
